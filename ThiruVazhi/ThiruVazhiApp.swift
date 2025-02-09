13d591318c49571a3e387f6145ca08de1dbc0487
Mohamed Fiyaz <mohamedfiyaz@gmail.com>
2025-02-09 15:24:17 +0530

    Initial Commit

---
+++ ThiruVazhi/ThiruVazhiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThiruVazhiApp.swift
+//  ThiruVazhi
+//
+//  Created by Mohamed Fiyaz on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ThiruVazhiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
