4ea424c8d078943ad884b8cd50b64c89c21de5d9
Mohamed Fiyaz <mohamedfiyaz@gmail.com>
2025-02-20 09:27:32 +0530

    Added history preview

--- ThiruVazhi/Components/HistoryCard.swift
+++ ThiruVazhi/Components/HistoryCard.swift
@@ -27,10 +27,9 @@ struct HistoryCard: View {
                     .foregroundColor(.black)
                 
                 VStack(spacing: 12) {
-                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                    Text("Thirukkural, written by the revered poet Thiruvalluvar over 2,000 years ago, is a timeless Tamil classic. Comprising 1,330 couplets, it offers wisdom on virtue, wealth, and love. Its universal values have made it a cornerstone of ethics and morality.")
                         .font(.system(size: fontSize(16)))
                         .foregroundColor(.black)
-                        .multilineTextAlignment(.center)
                         .padding(20)
                     NavigationLink(destination: HistoryDetailView(scrollProxy: $historyScrollProxy)) {
 
